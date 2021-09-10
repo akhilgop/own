a4190048c926302c1e16869904e029a67efc8527
akhilgop <akh_il@hotmail.com>
2021-09-10 19:56:08 +0530

    V1.6
    * Animations added
    * Horizontal Scroll added

--- js/custom.js
+++ js/custom.js
@@ -1,89 +1,231 @@
 // ---------------------------------------------------------------------Navbar------------------------------------------------------------------
-var nav = document.querySelector('nav'); // Identify target
-window.addEventListener('scroll', function (event) { // To listen for event
-    event.preventDefault();
+const showAnim = gsap.from('.nb', {
+    yPercent: -100,
+    paused: true,
+    duration: 0.2
+}).progress(1);
 
-    if (window.scrollY <= 150) {
-        nbb.style.color = '#fff';
-        nav.style.boxShadow = 'none !important';
-    } else {
-        nav.style.backgroundColor = '#fff';
-        nav.style.boxShadow = '0px 1px 6px rgba(0, 0, 0, 0.1)';
-        nav.style.height = '80px';
+ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+        self.direction === -1 ? showAnim.play() : showAnim.reverse()
     }
 });
 
 
+//---------------------------------------------------------------------Animate on Scroll------------------------------------------------------------------
+function animateFrom(elem, direction) {
+    // 'use strict';
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    elem.style.transform = "translate(" + x + "px, " + y + "px)";
+    elem.style.opacity = "0";
+    gsap.fromTo(elem, { x: x, y: y, autoAlpha: 0 }, {
+        duration: 1.25,
+        x: 0,
+        y: 0,
+        autoAlpha: 1,
+        ease: "expo",
+        overwrite: "auto"
+    });
+}
 
-var nav = document.querySelector('nav'); // Identify target
-window.addEventListener('scroll', function (event) { // To listen for event
-    event.preventDefault();
+function hide(elem) {
+    gsap.set(elem, { autoAlpha: 0 });
+}
 
-    if (window.scrollY == 0) {
-        nav.style.boxShadow = 'none';
-        nav.style.height = '150px';
-    } else {
-        nav.style.backgroundColor = '#fff';
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
 
-    }
+    gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
+        hide(elem); // assure that the element is hidden when scrolled into view
+
+        ScrollTrigger.create({
+            trigger: elem,
+            onEnter: function () { animateFrom(elem) },
+            onEnterBack: function () { animateFrom(elem, -1) },
+            onLeave: function () { hide(elem) } // assure that the element is hidden when scrolled into view
+        });
+    });
 });
 
-// ---------------------------------------------------------------------Scrollbar------------------------------------------------------------------
+
+// ---------------------------------------------------------------------Image Reveal---------------------------------------------------------------
 gsap.registerPlugin(ScrollTrigger);
 
-const pageContainer = document.querySelector(".scroll-wrapper");
+let revealContainers = document.querySelectorAll(".reveal");
 
-/* SMOOTH SCROLL */
-const scroller = new LocomotiveScroll({
-    el: pageContainer,
-    smooth: true,
+revealContainers.forEach((container) => {
+    let image = container.querySelector("img");
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: container,
+            toggleActions: "restart none none reset"
+        }
+    });
 
+    tl.set(container, { autoAlpha: 1 });
+    tl.from(container, 1.5, {
+        xPercent: -100,
+        ease: Power2.out
+    });
+    tl.from(image, 1.5, {
+        xPercent: 100,
+        scale: 1.3,
+        delay: -1.5,
+        ease: Power2.out
+    });
 });
 
-scroller.on("scroll", ScrollTrigger.update);
+// ---------------------------------------------------------------------Cursor------------------------------------------------------------------
+const updateProperties = (elem, state) => {
+    elem.style.setProperty('--x', `${state.x}px`)
+    elem.style.setProperty('--y', `${state.y}px`)
+    elem.style.setProperty('--width', `${state.width}px`)
+    elem.style.setProperty('--height', `${state.height}px`)
+    elem.style.setProperty('--radius', state.radius)
+    elem.style.setProperty('--scale', state.scale)
+}
 
-ScrollTrigger.scrollerProxy(pageContainer, {
-    scrollTop(value) {
-        return arguments.length
-            ? scroller.scrollTo(value, 0, 0)
-            : scroller.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
+document.querySelectorAll('.cursor').forEach(cursor => {
+    let onElement
+
+    const createState = e => {
+        const defaultState = {
+            x: e.clientX,
+            y: e.clientY,
+            width: 40,
+            height: 40,
+            radius: '50%'
+        }
+
+        const computedState = {}
+
+        if (onElement != null) {
+            const { top, left, width, height } = onElement.getBoundingClientRect()
+            const radius = window.getComputedStyle(onElement).borderTopLeftRadius
+
+            computedState.x = left + width / 2
+            computedState.y = top + height / 2
+            computedState.width = width
+            computedState.height = height
+            computedState.radius = radius
+        }
+
         return {
-            left: 0,
-            top: 0,
-            width: window.innerWidth,
-            height: window.innerHeight
-        };
-    },
-    pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
+            ...defaultState,
+            ...computedState
+        }
+    }
 
-////////////////////////////////////
-////////////////////////////////////
+    document.addEventListener('mousemove', e => {
+        const state = createState(e)
+        updateProperties(cursor, state)
+    })
+
+    // document.querySelectorAll('a, button').forEach(elem => {
+    //     elem.addEventListener('mouseenter', () => (onElement = elem))
+    //     elem.addEventListener('mouseleave', () => (onElement = undefined))
+    // })
+})
+
+// ---------------------------------------------------------------------Full Screen Section--------------------------------------------------------------
+// ---------------------------------------------------------------------Locomotive Scroll--------------------------------------------------------------
 window.addEventListener("load", function () {
-    let pinBoxes = document.querySelectorAll(".pin-wrap > *");
-    let pinWrap = document.querySelector(".pin-wrap");
-    let pinWrapWidth = pinWrap.offsetWidth;
-    let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+    gsap.registerPlugin(ScrollTrigger);
+
+    const pageContainer = document.querySelector(".scroll-wrapper");
+    pageContainer.setAttribute("data-scroll-container", "");
+
+    const scroller = new LocomotiveScroll({
+        el: pageContainer,
+        smooth: false,
+        getDirection: true
+    });
+
+
+    scroller.on("scroll", function (t) {
+        document.documentElement.setAttribute("data-direction", t.direction);
+    });
+
+    scroller.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(pageContainer, {
+        scrollTop(value) {
+            return arguments.length ?
+                scroller.scrollTo(value, 0, 0) :
+                scroller.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                left: 0,
+                top: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+
+        },
+        pinType: pageContainer.style.transform ? "transform" : "fixed"
+    });
+
 
     // Pinning and horizontal scrolling
 
-    gsap.to(".pin-wrap", {
-        scrollTrigger: {
-            scroller: pageContainer, //locomotive-scroll
-            scrub: true,
-            trigger: "#sectionPin",
-            pin: true,
-            // anticipatePin: 1,
-            start: "top top",
-            end: pinWrapWidth
-        },
-        x: -horizontalScrollLength,
-        ease: "none"
+    let horizontalSections = document.querySelectorAll(".horizontal-scroll");
+
+    horizontalSections.forEach(horizontalSection => {
+        let pinWrap = horizontalSection.querySelector(".pin-wrap");
+        let pinWrapWidth = pinWrap.offsetWidth;
+        let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+        gsap.to(pinWrap, {
+            scrollTrigger: {
+                scroller: "[data-scroll-container]",
+                scrub: true,
+                trigger: horizontalSection,
+                pin: true,
+                start: "top top",
+                end: () => `+=${pinWrapWidth}`,
+                invalidateOnRefresh: true
+            },
+
+            x: -horizontalScrollLength,
+            ease: "none"
+        });
+
     });
 
-    ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+    /* COLOR CHANGER */
+
+    const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
+    scrollColorElems.forEach((colorSection, i) => {
+        const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
+        const prevText = i === 0 ? "" : scrollColorElems[i - 1].dataset.textcolor;
+
+        ScrollTrigger.create({
+            trigger: colorSection,
+            scroller: "[data-scroll-container]",
+            start: "top 50%",
+            onEnter: () =>
+                gsap.to("body", {
+                    backgroundColor: colorSection.dataset.bgcolor,
+                    color: colorSection.dataset.textcolor,
+                    overwrite: "auto"
+                }),
+
+            onLeaveBack: () =>
+                gsap.to("body", {
+                    backgroundColor: prevBg,
+                    color: prevText,
+                    overwrite: "auto"
+                })
+        });
+
+
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => scroller.update());
 
     ScrollTrigger.refresh();
 });
